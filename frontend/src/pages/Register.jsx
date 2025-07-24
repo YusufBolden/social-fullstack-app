d1861880672650357cb0d7ac9cba247998728f15
Yusuf Bolden <ajb2316@columbia.edu>
2025-07-24 14:57:10 -0400

    Changes login endpoint

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -25,7 +25,7 @@ function RegisterPage() {
         console.log(res);
 
         localStorage.setItem('social-app-token', JSON.stringify(res.data.token))
-        navigate('/signin')
+        navigate('/login')
     } catch (error) {
         console.log(error);     
     }
